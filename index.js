4d4d396828f9f31c3a6f44f1ed5dab3ad5dcbd1c
jgeringer <joegeringer@gmail.com>
2022-12-22 20:30:51 -0600

    attacks

--- index.js
+++ index.js
@@ -10,20 +10,41 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'red', offset }) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color
+        this.isAttacking
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, 50, this.height, this.width)
+
+        // attack box
+        if (this.isAttacking) {
+            c.fillStyle = 'green'
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        }
     }
 
     update() {
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
+
         this.position.y += this.velocity.y
         this.position.x += this.velocity.x
 
@@ -32,6 +53,13 @@ class Sprite {
         } else {
             this.velocity.y += gravity
         }
+    }
+
+    attack() {
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100)
     }
 }
 
@@ -43,6 +71,10 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 10
+    },
+    offset: {
+        x: 0,
+        y: 0
     }
 })
 
@@ -54,7 +86,12 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    offset: {
+        x: -50,
+        y: 0
+    },
+    color: 'blue'
 })
 
 console.log(player)
@@ -72,6 +109,15 @@ const keys = {
     ArrowLeft: {
         pressed: false
     }
+}
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
 }
 
 function animate() {
@@ -98,12 +144,30 @@ function animate() {
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
     }
+
+    // detect for collision
+    if (rectangularCollision({
+        rectangle1: player,
+        rectangle2: enemy
+    }) && 
+        player.isAttacking) {
+        console.log('player hit enemy')
+        player.isAttacking = false
+    }
+
+    if (rectangularCollision({
+        rectangle1: enemy,
+        rectangle2: player
+    }) && 
+        enemy.isAttacking) {
+        console.log('enemy hit player')
+        enemy.isAttacking = false
+    }
 }
 
 animate()
 
 window.addEventListener('keydown', (event) => {
-    console.log(event.key)
     switch (event.key) {
         // player...
         case 'd':
@@ -117,6 +181,10 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -20
         break
+        case ' ':
+            player.attack()
+        break
+
         // enemy...
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
@@ -129,11 +197,13 @@ window.addEventListener('keydown', (event) => {
         case 'ArrowUp':
             enemy.velocity.y = -20
         break
+        case 'ArrowDown':
+            enemy.isAttacking = true
+        break
     }
 })
 
 window.addEventListener('keyup', (event) => {
-    console.log(event.key)
     switch (event.key) {
         case 'd':
             keys.d.pressed = false
