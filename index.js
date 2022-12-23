2c5263c211f960b529d32835d96cbdf97c74c0f9
jgeringer <joegeringer@gmail.com>
2022-12-22 22:15:56 -0600

    timers and game over

--- index.js
+++ index.js
@@ -121,6 +121,37 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    const displayText = document.querySelector('#displayText')
+    if (player.health === enemy.health) {
+        displayText.innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        displayText.innerHTML = 'Player 1 Wins'
+    } else if (player.health < enemy.health) {
+        displayText.innerHTML = 'Player 2 Wins'
+    }
+    displayText.style.display = 'flex'
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('#timer').innerHTML = timer
+        console.log('player health', player.health)
+    }
+
+    if (timer === 0) {
+        determineWinner({ player, enemy, timerId })
+    }
+
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -167,6 +198,11 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
     }
 }
 
