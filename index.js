4445cd0f18bb040ed5d8f9346b5dca028f0172f7
jgeringer <joegeringer@gmail.com>
2022-12-22 21:00:46 -0600

    health bar stats

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Sprite {
         }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -153,6 +154,8 @@ function animate() {
         player.isAttacking) {
         console.log('player hit enemy')
         player.isAttacking = false
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (rectangularCollision({
@@ -162,6 +165,8 @@ function animate() {
         enemy.isAttacking) {
         console.log('enemy hit player')
         enemy.isAttacking = false
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
