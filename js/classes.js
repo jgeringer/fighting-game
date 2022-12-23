85b575dae4fec898d11dbda75a7f335ad5b6e9f3
jgeringer <joegeringer@gmail.com>
2022-12-22 23:05:08 -0600

    shop animation

--- js/classes.js
+++ js/classes.js
@@ -1,18 +1,46 @@
 class Sprite {
-    constructor({ position, imageSrc }) {
+    constructor({ position, imageSrc, scale = 1, framesMax = 1 }) {
         this.position = position
         this.width = 50
         this.height = 150
         this.image = new Image()
         this.image.src = imageSrc
+        this.scale = scale
+        this.framesMax = framesMax
+        this.framesCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
     }
 
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.drawImage(
+            this.image,
+
+            // this is how canvas crops, has to start at the second arg here
+            this.framesCurrent * (this.image.width / this.framesMax), // x crop location
+            0,
+            this.image.width / this.framesMax, // divided by frames
+            this.image.height,
+
+            this.position.x, 
+            this.position.y, 
+            this.image.width / this.framesMax * this.scale, 
+            this.image.height * this.scale
+        )
     }
 
     update() {
         this.draw()
+        this.framesElapsed++
+
+        if (this.framesElapsed % this.framesHold === 0) {
+            if (this.framesCurrent < this.framesMax - 1) {
+                this.framesCurrent++
+            } else {
+                this.framesCurrent = 0
+            }
+        }
+
     }
 }
 
